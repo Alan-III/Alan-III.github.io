4e706c1068cb2f12eeff0f67ad45ad98a4e419c5
Alan-III <alexander_grt@hotmail.com>
2023-10-24 23:53:52 +0300

    typewriter

---
+++ js/typewriter.js
@@ -0,0 +1,31 @@
+const words = ['Software Developer.', 'Game Developer.'];
+let currentWord = 0;
+let currentLetter = 0;
+let erasing = false;
+
+const typewriterElement = document.getElementById('typewriter');
+
+function typeWriter() {
+  if (!erasing) {
+    if (currentLetter < words[currentWord].length) {
+      typewriterElement.textContent += words[currentWord][currentLetter];
+      currentLetter++;
+      setTimeout(typeWriter, 100);
+    } else {
+      erasing = true;
+      setTimeout(typeWriter, 2000);
+    }
+  } else {
+    if (currentLetter > 0) {
+      typewriterElement.textContent = words[currentWord].substring(0, currentLetter - 1);
+      currentLetter--;
+      setTimeout(typeWriter, 50);
+    } else {
+      erasing = false;
+      currentWord = (currentWord + 1) % words.length;
+      setTimeout(typeWriter, 500);
+    }
+  }
+}
+
+setTimeout(typeWriter, 1000);
